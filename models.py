8f90b23cc896c2f8a4e93d240033425dbf27ead2
svs <sailesh101195@gmail.com>
2017-11-06 10:50:51 -0600

    final

--- models.py
+++ models.py
@@ -43,9 +43,15 @@ def train_ffnn(train_exs, dev_exs, test_exs, word_vectors):
 
     keep_prob = tf.placeholder(tf.float32)
 
-    embeddings = tf.cast(tf.nn.embedding_lookup(word_vectors.vectors, x), tf.float32)
+    seq = tf.placeholder(tf.int32, [None])
+    embed_mat_trainable = tf.get_variable("word_vectors", dtype=tf.float32, initializer=tf.constant(word_vectors.vectors, dtype=tf.float32))
+    embeddings = tf.cast(tf.nn.embedding_lookup(embed_mat_trainable, x), tf.float32)
     ns = [tf.shape(embeddings)[0], seq_max_len, 1]
-    input_data = tf.reduce_mean(tf.nn.dropout(embeddings, keep_prob, noise_shape=ns), 1)
+    embeddings = tf.nn.dropout(embeddings, keep_prob, noise_shape=ns)
+    input_data = tf.reduce_mean(embeddings, 1)
+    seq1 = tf.reshape(tf.divide(seq_max_len, tf.cast(seq, dtype=tf.float32)), [-1, 1])
+    seq1 = tf.tile(seq1, [1, feat_vec_size])
+    input_data = tf.multiply(input_data, seq1)
     y_one_hot = tf.one_hot(y_, depth=num_labels)
 
     num_h1 = 300
@@ -57,6 +63,7 @@ def train_ffnn(train_exs, dev_exs, test_exs, word_vectors):
 
         out = tf.add(tf.matmul(input, W), b)
         if not final:
+            out = tf.nn.dropout(out, keep_prob)
             out = tf.nn.relu(out)
 
         return out
@@ -67,19 +74,28 @@ def train_ffnn(train_exs, dev_exs, test_exs, word_vectors):
 
     loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y_one_hot))
 
+    train_vars = tf.trainable_variables()
+
     decay_steps = 100
     learning_rate_decay_factor = 0.99
     global_step = tf.train.get_or_create_global_step()
     # Smaller learning rates are sometimes necessary for larger networks
     initial_learning_rate = 0.001
     # Decay the learning rate exponentially based on the number of steps.
-    lr = tf.train.exponential_decay(initial_learning_rate,
+    lr1 = tf.train.exponential_decay(initial_learning_rate,
+                                    global_step,
+                                    decay_steps,
+                                    learning_rate_decay_factor,
+                                    staircase=True)
+    lr2 = tf.train.exponential_decay(initial_learning_rate*0.5,
                                     global_step,
                                     decay_steps,
                                     learning_rate_decay_factor,
                                     staircase=True)
 
-    optimizer = tf.train.AdamOptimizer(learning_rate=initial_learning_rate).minimize(loss)
+    optimizer1 = tf.train.AdamOptimizer(learning_rate=lr1).minimize(loss)
+    # optimizer1 = tf.train.AdamOptimizer(learning_rate=lr1).minimize(loss, var_list=train_vars[1:])
+    # optimizer2 = tf.train.AdamOptimizer(learning_rate=lr2).minimize(loss, var_list=train_vars[0:1])
 
 
     predictions = tf.argmax(pred, 1)
@@ -88,7 +104,7 @@ def train_ffnn(train_exs, dev_exs, test_exs, word_vectors):
 
     init = tf.global_variables_initializer()
 
-    total_epochs = 15
+    total_epochs = 50
 
     batch_size = 15
     splits = train_size/batch_size
@@ -103,6 +119,7 @@ def train_ffnn(train_exs, dev_exs, test_exs, word_vectors):
 
         batches_x = np.split(train_mat, batch_sizes)
         batches_y = np.split(train_labels_arr, batch_sizes)
+        batches_l = np.split(train_seq_lens, batch_sizes)
         
         for epoch in range(total_epochs):
 
@@ -110,10 +127,11 @@ def train_ffnn(train_exs, dev_exs, test_exs, word_vectors):
             for idx in range(len(batch_sizes)):
                 batch_x = batches_x[idx]
                 batch_y = batches_y[idx]
+                batch_l = batches_l[idx]
 
-                _, l = sess.run([optimizer, loss], feed_dict={x: batch_x, y_: batch_y, keep_prob: 0.9})
+                _, l = sess.run([optimizer1, loss], feed_dict={x: batch_x, y_: batch_y, seq: batch_l, keep_prob: 0.7})
 
-                avg_loss += l / batch_size
+                avg_loss += l / len(batch_sizes)
             
                 # if idx % 100 == 0:
                 #     print('Epoch : %d, Batch : %d, loss : %.3f' % (epoch+1, idx, l))
@@ -121,11 +139,11 @@ def train_ffnn(train_exs, dev_exs, test_exs, word_vectors):
             print('Epoch : %d, avg loss : %.3f' % (epoch+1, avg_loss))
 
             # if epoch % 5 == 0:
-            t_acc = sess.run(accuracy, feed_dict={x: train_mat, y_: train_labels_arr, keep_prob: 1.0})
-            v_acc = sess.run(accuracy, feed_dict={x: dev_mat, y_: dev_labels_arr, keep_prob: 1.0})
-            print('Train accuracy : %.3f, Valid accuracy : %.3f' % (t_acc, v_acc))
+            t_acc = sess.run(accuracy, feed_dict={x: train_mat, y_: train_labels_arr, seq: train_seq_lens, keep_prob: 1.0})
+            v_acc = sess.run(accuracy, feed_dict={x: dev_mat, y_: dev_labels_arr, seq: dev_seq_lens, keep_prob: 1.0})
+            print('Train accuracy : %.3f%%, Valid accuracy : %.3f%%' % (t_acc*100, v_acc*100))
 
-        test_pred = sess.run(predictions, feed_dict={x: test_mat, keep_prob: 1.0})
+        test_pred = sess.run(predictions, feed_dict={x: test_mat, seq: test_seq_lens, keep_prob: 1.0})
 
     for idx, ex in enumerate(test_exs):
         ex.label = test_pred[idx]
@@ -158,24 +176,28 @@ def train_fancy(train_exs, dev_exs, test_exs, word_vectors):
 
     keep_prob = tf.placeholder(tf.float32)
 
-    embeddings = tf.cast(tf.nn.embedding_lookup(word_vectors.vectors, x), tf.float32)
+    embed_mat_trainable = tf.get_variable("word_vectors", dtype=tf.float32, initializer=tf.constant(word_vectors.vectors, dtype=tf.float32))
+    embeddings = tf.cast(tf.nn.embedding_lookup(embed_mat_trainable, x), tf.float32)
     y_one_hot = tf.one_hot(y_, depth=num_labels)
 
 
-    lstmUnits = 100
+    lstmUnits = 50
 
     seq = tf.placeholder(tf.int32, [None])
 
-    lstm_fw = tf.contrib.rnn.BasicLSTMCell(lstmUnits)
-    lstm_bw = tf.contrib.rnn.BasicLSTMCell(lstmUnits)
-    # lstm = tf.contrib.rnn.DropoutWrapper(cell=lstm, output_keep_prob=0.75)
-    # rnn_outputs, _ = tf.nn.dynamic_rnn(lstm, embeddings, sequence_length=seq, dtype=tf.float32)
+    lstm_fw = tf.contrib.rnn.GRUCell(lstmUnits)
+    lstm_fw = tf.contrib.rnn.DropoutWrapper(cell=lstm_fw, output_keep_prob=keep_prob)
+    lstm_bw = tf.contrib.rnn.GRUCell(lstmUnits)
+    lstm_bw = tf.contrib.rnn.DropoutWrapper(cell=lstm_bw, output_keep_prob=keep_prob)
+    # rnn_outputs, _ = tf.nn.dynamic_rnn(lstm_fw, embeddings, sequence_length=seq, dtype=tf.float32)
     rnn_outputs, _, _ = tf.contrib.rnn.stack_bidirectional_dynamic_rnn([lstm_fw], [lstm_bw], embeddings, sequence_length=seq, dtype=tf.float32)
 
 
     # rnn_outputs = tf.transpose(rnn_outputs, [1, 0, 2])
     gather_idx = tf.placeholder(tf.int32, [None, 2])
     input_data = tf.gather_nd(rnn_outputs, gather_idx)
+    # input_data = tf.reshape(input_data, [-1, 2, lstmUnits])
+    # input_data = tf.reduce_prod(input_data, 1)
 
     num_h1 = 100
     num_h2 = 300
@@ -196,19 +218,27 @@ def train_fancy(train_exs, dev_exs, test_exs, word_vectors):
 
     loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y_one_hot))
 
+    trainable_var = tf.trainable_variables()
+
     decay_steps = 100
     learning_rate_decay_factor = 0.99
     global_step = tf.train.get_or_create_global_step()
     # Smaller learning rates are sometimes necessary for larger networks
-    initial_learning_rate = 0.001
+    initial_learning_rate = 0.01
     # Decay the learning rate exponentially based on the number of steps.
-    lr = tf.train.exponential_decay(initial_learning_rate,
+    lr1 = tf.train.exponential_decay(initial_learning_rate,
+                                    global_step,
+                                    decay_steps,
+                                    learning_rate_decay_factor,
+                                    staircase=True)
+    lr2 = tf.train.exponential_decay(initial_learning_rate*0.1,
                                     global_step,
                                     decay_steps,
                                     learning_rate_decay_factor,
                                     staircase=True)
 
-    optimizer = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss)
+    optimizer1 = tf.train.AdamOptimizer(learning_rate=lr1).minimize(loss, var_list=trainable_var[1:])
+    optimizer2 = tf.train.AdamOptimizer(learning_rate=lr2).minimize(loss, var_list=trainable_var[0:1])
 
 
     predictions = tf.argmax(pred, 1)
@@ -217,9 +247,9 @@ def train_fancy(train_exs, dev_exs, test_exs, word_vectors):
 
     init = tf.global_variables_initializer()
 
-    total_epochs = 20
+    total_epochs = 1
 
-    batch_size = 15
+    batch_size = 30
     splits = train_size/batch_size
     batch_sizes = (np.arange(splits-1, dtype=np.int32) * batch_size) + batch_size
     if splits*batch_size < train_size:
@@ -251,15 +281,15 @@ def train_fancy(train_exs, dev_exs, test_exs, word_vectors):
                 gather_list = get_gather_indices(batch_l)
                 # print(gather_list)
 
-                _, l, r, i = sess.run([optimizer, loss, rnn_outputs, input_data], feed_dict={x: batch_x, y_: batch_y, \
-                                                            seq: batch_l, gather_idx: gather_list, keep_prob: 0.9})
+                _, _, l = sess.run([optimizer1, optimizer2, loss], feed_dict={x: batch_x, y_: batch_y, \
+                                                            seq: batch_l, gather_idx: gather_list, keep_prob: 1.0})
 
                 # print(r)
                 # print(i)
                 avg_loss += l / len(batch_sizes)
             
-                if idx % 100 == 0:
-                    print('Epoch : %d, Batch : %d, loss : %.3f' % (epoch+1, idx, l))
+                # if idx % 100 == 0:
+                #     print('Epoch : %d, Batch : %d, loss : %.3f' % (epoch+1, idx, l))
 
             print('Epoch : %d, avg loss : %.3f' % (epoch+1, avg_loss))
 
